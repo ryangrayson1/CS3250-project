9bea2143ef3fa7094cf40e33ce1ee39d343800ff
ryangrayson1 <ryangrayson20@gmail.com>
2023-11-25 12:11:56 -0500

    ford fulkerson and edmonds karp functions

--- utils.py
+++ utils.py
@@ -4,17 +4,15 @@ from selenium.webdriver.common.by import By
 
 
 class Graph:
-    def __init__(self):
-        self.nodes = set()
+    def __init__(self, n):
+        self.n = n
         self.edges = []
 
     def add_edge(self, u, v, w):
-        self.nodes.add(u)
-        self.nodes.add(v)
         self.edges.append((u, v, w))
 
     def __str__(self):
-        s = f"{max(self.nodes) + 1} {len(self.edges)}\n"
+        s = f"{self.n} {len(self.edges)}\n"
         for u, v, w in self.edges:
             s += f"{u} {v} {w}\n"
         return s
@@ -38,7 +36,7 @@ class SiteManager:
         overlay = self.driver.find_element(By.ID, "overlay")
         cw(overlay)
 
-    def open_graph_input(self):
+    def open_graph_input(self, default=False):
         edit_graph_button = self.driver.find_element(By.ID, "draw")
         cw(edit_graph_button)
         graph_input_button = self.driver.find_element(By.ID, "graph-input")
@@ -48,10 +46,11 @@ class SiteManager:
         graph_type.find_element(By.XPATH, "//input[@value='0-Index']").click()
         # set the type to flow
         graph_type = self.driver.find_element(By.NAME, "graph-drawing-type")
-        cw(graph_type.find_element(By.XPATH, "//input[@value='Flow']"))
+        if not default:
+            cw(graph_type.find_element(By.XPATH, f"//input[@value='Flow']"))
 
     # assumes graph input field is already open
-    def set_graph(self, graph_str):
+    def set_graph(self, graph_str, click_done=True):
         # set the text in the graph input field
         graph_spec = self.driver.find_element(By.ID, "graph-input-field")
         graph_spec.clear()
@@ -62,8 +61,14 @@ class SiteManager:
             By.XPATH, "//button[contains(@onclick, 'create_graph(true)')]"
         )
         cw(submit_button)
-        done_button = self.driver.find_element(By.CLASS_NAME, "done-button")
-        cw(done_button)
+        if click_done:
+            done_button = self.driver.find_element(By.CLASS_NAME, "done-button")
+            cw(done_button)
+
+    def get_draw_error(self):
+        editor = self.driver.find_element(By.ID, "main")
+        err = editor.find_element(By.ID, "draw-err")
+        return err.find_element(By.TAG_NAME, "p").text
 
     def ford_fulkerson(self, s, t):
         ff = self.driver.find_element(By.ID, "fordfulkerson")
@@ -82,6 +87,39 @@ class SiteManager:
         max_flow = result.split(".")[0][-1]
         return int(max_flow)
 
+    def run_ford_fulkerson(self, graph_str, s, t):
+        self.open_graph_input()
+        self.set_graph(graph_str)
+        return self.ford_fulkerson(s, t)
+
+    def get_ford_fulkerson_error(self):
+        return self.driver.find_element(By.ID, "fordfulkerson-err").text
+
+    def edmonds_karp(self, s, t):
+        ek = self.driver.find_element(By.ID, "edmondskarp")
+        cw(ek)
+        source = self.driver.find_element(By.ID, "edmondskarp-sourcevertex")
+        source.clear()
+        source.send_keys(s)
+        sink = self.driver.find_element(By.ID, "edmondskarp-sinkvertex")
+        sink.clear()
+        sink.send_keys(t)
+        go = self.driver.find_element(By.ID, "edmondskarp-go")
+        cw(go)
+        finish = self.driver.find_element(By.ID, "go-to-end")
+        cw(finish)
+        result = self.driver.find_element(By.ID, "status").text
+        max_flow = result.split(".")[0][-1]
+        return int(max_flow)
+
+    def run_edmonds_karp(self, graph_str, s, t):
+        self.open_graph_input()
+        self.set_graph(graph_str)
+        return self.edmonds_karp(s, t)
+
+    def get_edmonds_karp_error(self):
+        return self.driver.find_element(By.ID, "edmondskarp-err").text
+
     def dinics(self, s, t):
         dinic = self.driver.find_element(By.ID, "dinic")
         cw(dinic)
@@ -98,19 +136,6 @@ class SiteManager:
         result = self.driver.find_element(By.ID, "status").text
         max_flow = result.split(".")[0][-1]
         return int(max_flow)
-
-    def run_ford_fulkerson(self, graph_str, s, t):
-        self.open_graph_input()
-        self.set_graph(graph_str)
-        return self.ford_fulkerson(s, t)
-
-    def get_ford_fulkerson_error(self):
-        return self.driver.find_element(By.ID, "fordfulkerson-err").text
-
-    def run_edmonds_karp(self, graph_str, s, t):
-        self.open_graph_input()
-        self.set_graph(graph_str)
-        return self.edmonds_karp(s, t)
 
     def run_dinics(self, graph_str, s, t):
         self.open_graph_input()
