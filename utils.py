fb1bcd0f3b7bce6810cba415f7d77153aadcda87
ryangrayson1 <ryangrayson20@gmail.com>
2023-11-25 11:01:56 -0500

    utils click and wait update

--- utils.py
+++ utils.py
@@ -20,11 +20,14 @@ class Graph:
         return s
 
 
-WAIT = .5
-# do and wait for page load
-def dw(fn):
-    fn()
+WAIT = 0.5
+
+
+# click and wait for page load
+def cw(elem):
+    elem.click()
     time.sleep(WAIT)
+
 
 class SiteManager:
     def __init__(self, url):
@@ -33,19 +36,19 @@ class SiteManager:
 
     def close_instructions(self):
         overlay = self.driver.find_element(By.ID, "overlay")
-        dw(overlay.click)
+        cw(overlay)
 
     def open_graph_input(self):
         edit_graph_button = self.driver.find_element(By.ID, "draw")
-        dw(edit_graph_button.click)
+        cw(edit_graph_button)
         graph_input_button = self.driver.find_element(By.ID, "graph-input")
-        dw(graph_input_button.click)
+        cw(graph_input_button)
         # set to 0-indexed
         graph_type = self.driver.find_element(By.NAME, "indexing-option")
         graph_type.find_element(By.XPATH, "//input[@value='0-Index']").click()
         # set the type to flow
         graph_type = self.driver.find_element(By.NAME, "graph-drawing-type")
-        dw(graph_type.find_element(By.XPATH, "//input[@value='Flow']").click)
+        cw(graph_type.find_element(By.XPATH, "//input[@value='Flow']"))
 
     # assumes graph input field is already open
     def set_graph(self, graph_str):
@@ -55,14 +58,33 @@ class SiteManager:
         graph_spec.send_keys(graph_str)
         # submit this graph
         graph_options = self.driver.find_element(By.ID, "graph-input-options")
-        submit_button = graph_options.find_element(By.XPATH, "//button[contains(@onclick, 'create_graph(true)')]")
-        dw(submit_button.click)
+        submit_button = graph_options.find_element(
+            By.XPATH, "//button[contains(@onclick, 'create_graph(true)')]"
+        )
+        cw(submit_button)
         done_button = self.driver.find_element(By.CLASS_NAME, "done-button")
-        dw(done_button.click)
+        cw(done_button)
+
+    def ford_fulkerson(self, s, t):
+        ff = self.driver.find_element(By.ID, "fordfulkerson")
+        cw(ff)
+        source = self.driver.find_element(By.ID, "fordfulkerson-sourcevertex")
+        source.clear()
+        source.send_keys(s)
+        sink = self.driver.find_element(By.ID, "fordfulkerson-sinkvertex")
+        sink.clear()
+        sink.send_keys(t)
+        go = self.driver.find_element(By.ID, "fordfulkerson-go")
+        cw(go)
+        finish = self.driver.find_element(By.ID, "go-to-end")
+        cw(finish)
+        result = self.driver.find_element(By.ID, "status").text
+        max_flow = result.split(".")[0][-1]
+        return int(max_flow)
 
     def dinics(self, s, t):
         dinic = self.driver.find_element(By.ID, "dinic")
-        dw(dinic.click)
+        cw(dinic)
         source = self.driver.find_element(By.ID, "dinic-sourcevertex")
         source.clear()
         source.send_keys(s)
@@ -70,12 +92,25 @@ class SiteManager:
         sink.clear()
         sink.send_keys(t)
         go = self.driver.find_element(By.ID, "dinic-go")
-        dw(go.click)
+        cw(go)
         finish = self.driver.find_element(By.ID, "go-to-end")
-        dw(finish.click)
+        cw(finish)
         result = self.driver.find_element(By.ID, "status").text
         max_flow = result.split(".")[0][-1]
         return int(max_flow)
+
+    def run_ford_fulkerson(self, graph_str, s, t):
+        self.open_graph_input()
+        self.set_graph(graph_str)
+        return self.ford_fulkerson(s, t)
+
+    def get_ford_fulkerson_error(self):
+        return self.driver.find_element(By.ID, "fordfulkerson-err").text
+
+    def run_edmonds_karp(self, graph_str, s, t):
+        self.open_graph_input()
+        self.set_graph(graph_str)
+        return self.edmonds_karp(s, t)
 
     def run_dinics(self, graph_str, s, t):
         self.open_graph_input()
